e2e91bc5c61bc05f6f0b5fdc3f181276b78bd6be
aza0092 <31869379+aza0092@users.noreply.github.com>
2018-09-26 19:11:53 -0500

    Create Form2.cs

---
+++ GetBit Project/Form2.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GetBit_Project
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+                Form1 f1 = new Form1();
+            f1.ShowDialog();
+        }
+    }
+}
